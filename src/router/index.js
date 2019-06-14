fd45cca7a88c312bbeafb40ea5868bb0eebd8db7
youht88 <1742716458@qq.com>
2019-06-14 07:57:37 +0000

    dev2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dev2 from '@/components/Dev2'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/dev2',
+      name: 'Dev2',
+      component: Dev2
+    },
   ]
 })
