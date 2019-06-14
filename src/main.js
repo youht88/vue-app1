f6c5e8f3e41dc3e60d809114644098fc3a199c1f
youht88 <1742716458@qq.com>
2019-06-14 07:54:54 +0000

    dev1

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
-import ElementUI from 'element-aui'
+// import ElementUI from 'element-aui'
 
 import Vue from 'vue'
+import store from 'vuex1'
 import App from './App'
 import router from './router'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Dev1 from '@/components/Dev1'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
+    {
+      path: '/dev1',
+      name: 'Dev1',
+      component: Dev1
+    },
   ]
 })
